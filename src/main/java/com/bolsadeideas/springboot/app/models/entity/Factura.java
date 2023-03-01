171d2cebee8229aaabb96303d87abb11a3e59ed2
AnthMKN <atonic7@gmai.com>
2023-03-01 11:44:05 +0100

    Facturas v2

--- src/main/java/com/bolsadeideas/springboot/app/models/entity/Factura.java
+++ src/main/java/com/bolsadeideas/springboot/app/models/entity/Factura.java
@@ -1,17 +1,22 @@
 package com.bolsadeideas.springboot.app.models.entity;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.format.annotation.DateTimeFormat;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.Table;
 import jakarta.persistence.Temporal;
@@ -37,6 +42,14 @@ public class Factura implements Serializable{
 	
 	@ManyToOne(fetch = FetchType.LAZY)
 	private Cliente cliente;
+	
+	@OneToMany(fetch=FetchType.LAZY, cascade=CascadeType.ALL)
+	@JoinColumn(name="factura_id")
+	private List<ItemFactura> items;
+	
+	public Factura() {
+		this.items = new ArrayList<ItemFactura>();
+	}
 	
 	@PrePersist
 	public void prePersist() {
@@ -73,8 +86,31 @@ public class Factura implements Serializable{
 	public void setCliente(Cliente cliente) {
 		this.cliente = cliente;
 	}
+
+	public List<ItemFactura> getItems() {
+		return items;
+	}
+
+	public void setItems(List<ItemFactura> items) {
+		this.items = items;
+	}
 	
+	public void addItemFactura(ItemFactura item) {
+		this.items.add(item);
+	}
 	
+	public Double getTotal() {
+		Double total = 0.0;
+		
+		int size = items.size();
+		
+		for (int i = 0; i < size; i++) {
+			total += items.get(i).calcularImporte();
+		}
+		
+		return total;
+	}
 	
+	private static final long serialVersionUID = 1L;
 	
 }
