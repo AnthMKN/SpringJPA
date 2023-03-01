aad0c03ef9ca44721255a90e03c8ce1d3df04f11
asudonp <antoniosp@iescastelar.com>
2023-03-01 14:10:07 +0100

    add productos de prueba a import sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,9 +1,9 @@
 /* Populate tables */
 
-INSERT INTO clientes (id, nombre, apellido, email, foto, create_at) VALUES(1, 'AAJDHF', 'ASDA', 'ASDASD@JHG.COM', '', '2022-08-28');
-INSERT INTO clientes (id, nombre, apellido, email, foto, create_at) VALUES(2, 'AAJDHF', 'ASDA', 'ASDASD@JHG.COM', '','2022-08-28');
-INSERT INTO clientes (id, nombre, apellido, email, foto, create_at) VALUES(3, 'AAJDHF', 'ASDA', 'ASDASD@JHG.COM', '','2022-08-28');
-INSERT INTO clientes (id, nombre, apellido, email, foto, create_at) VALUES(4, 'AAJDHF', 'ASDA', 'ASDASD@JHG.COM', '','2022-08-28');
+INSERT INTO clientes (id, nombre, apellido, email, foto, create_at) VALUES(1, 'Paco', 'Merlo', 'paco@JHG.COM', '', '2022-08-28');
+INSERT INTO clientes (id, nombre, apellido, email, foto, create_at) VALUES(2, 'Maria', 'Porro', 'maria@JHG.COM', '','2022-08-28');
+INSERT INTO clientes (id, nombre, apellido, email, foto, create_at) VALUES(3, 'Dolores', 'Fuertes', 'dolores@JHG.COM', '','2022-08-28');
+INSERT INTO clientes (id, nombre, apellido, email, foto, create_at) VALUES(4, 'Rosa', 'Melano', 'rosa@JHG.COM', '','2022-08-28');
 INSERT INTO clientes (id, nombre, apellido, email, foto, create_at) VALUES(5, 'AAJDHF', 'ASDA', 'ASDASD@JHG.COM', '','2022-08-28');
 INSERT INTO clientes (id, nombre, apellido, email, foto, create_at) VALUES(6, 'AAJDHF', 'ASDA', 'ASDASD@JHG.COM', '','2022-08-28');
 INSERT INTO clientes (id, nombre, apellido, email, foto, create_at) VALUES(7, 'AAJDHF', 'ASDA', 'ASDASD@JHG.COM', '','2022-08-28');
@@ -16,3 +16,10 @@ INSERT INTO clientes (id, nombre, apellido, email, foto, create_at) VALUES(13, '
 INSERT INTO clientes (id, nombre, apellido, email, foto, create_at) VALUES(14, 'AAJDHF', 'ASDA', 'ASDASD@JHG.COM', '','2022-08-28');
 INSERT INTO clientes (id, nombre, apellido, email, foto, create_at) VALUES(15, 'Nacho', 'Segura', 'profesor@bolsadeideas.com', '', '2023-02-18');
 INSERT INTO clientes (id, nombre, apellido, email, foto, create_at) VALUES(16, 'John', 'Doe', 'john.doe@gmail.com', '', '2017-08-28');
+
+INSERT INTO productos (id, nombre, precio, create_at) VALUES(1,'Samsung S22',899,'2023-03-01');
+INSERT INTO productos (id, nombre, precio, create_at) VALUES(2,'IPhone 14 pro',1400,'2023-03-01');
+INSERT INTO productos (id, nombre, precio, create_at) VALUES(3,'Nintendo Switch',350,'2023-03-01');
+INSERT INTO productos (id, nombre, precio, create_at) VALUES(4,'PS5',550,'2023-03-01');
+INSERT INTO productos (id, nombre, precio, create_at) VALUES(5,'Asus VIVOBOOK',1099,'2023-03-01');
+INSERT INTO productos (id, nombre, precio, create_at) VALUES(6,'Razer Viper',120,'2023-03-01');
